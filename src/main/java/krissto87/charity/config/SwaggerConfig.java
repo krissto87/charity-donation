9615e9369e8f135c11789fed3817821c4f46f64e
krissto87 <krzysztof.janczak@onet.pl>
2020-01-14 09:53:02 +0100

    institutions rest api

---
+++ src/main/java/krissto87/charity/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package krissto87.charity.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Charity api")
+                .description("created by krissto87")
+                .version("1.0.0")
+                .build();
+    }
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("krissto87.charity"))
+                .build();
+    }
+}
---
+++ src/main/java/krissto87/charity/rest/InstitutionRestController.java
@@ -0,0 +1,38 @@
+package krissto87.charity.rest;
+
+import krissto87.charity.dtos.InstitutionDTO;
+import krissto87.charity.services.InstitutionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/institutions")
+public class InstitutionRestController {
+
+    private final InstitutionService institutionService;
+
+    public InstitutionRestController(InstitutionService institutionService) {
+        this.institutionService = institutionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<InstitutionDTO>> getAllInstitutions() {
+        List<InstitutionDTO> institutions = institutionService.findAllInstitutions();
+        return new ResponseEntity<>(institutions, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getInstitution(@PathVariable Long id) {
+        InstitutionDTO institution = institutionService.findById(id);
+        if (institution == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(institution, HttpStatus.OK);
+    }
+}
